120230d02e033dde089053a878a7feaf42a74f2e
Jaxeetee <66991639+Jaxeetee@users.noreply.github.com>
2022-10-28 17:21:33 +0800

    cue stick changes

--- Assets/Scripts/Gameplay/CueStick.cs
+++ Assets/Scripts/Gameplay/CueStick.cs
@@ -1,20 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CueStick : MonoBehaviour
 {
+    private Camera _main;
+
+    //for targetting
+    [SerializeField]
+    private Transform _cueBall;
 
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private float _mouseDragSpeed = 1f;
+
+    private Vector2 _setStartPosition;
+    private float _currentPos;
+    private bool _canMove;
+    private Vector3 _newPos;
+    private Vector3 velocity;
+
     void Start()
     {
-        
+        _main = Camera.main;   
+    }
+
+    private void OnEnable()
+    {
+        RollBall.onCollide += DisableThis;
+        InputManager.onMouseHold += DragThis;
+    }
+
+
+
+    private void OnDisable()
+    {
+        RollBall.onCollide -= DisableThis;
+        InputManager.onMouseHold -= DragThis;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputManager.Instance.MouseDeltaMovement(ref _setStartPosition);
+        _currentPos = _setStartPosition.x;
+
+        if (_canMove)
+        {
+            _newPos = new Vector3 (transform.position.x, -_currentPos, transform.position.z);
+        }
+
     }
+    private void DragThis()
+    {
+        Ray ray = _main.ScreenPointToRay(InputManager.Instance.GetMousePosition());
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null && hit.collider.name == this.gameObject.name)
+            {
+                StartCoroutine(IDragging(hit.collider.gameObject));
+            }
+        }
+    }
+
+
+    private IEnumerator IDragging(GameObject thisObject)
+    {
+        float initialDistance = Vector3.Distance(thisObject.transform.position, _main.transform.position);
+        while (true)
+        {
+            Ray ray = _main.ScreenPointToRay(InputManager.Instance.GetMousePosition());
+            thisObject.transform.position = Vector3.SmoothDamp(thisObject.transform.position, _newPos, ref velocity, _mouseDragSpeed);
+            yield return null;
+        }
+    }
+    private void DisableThis()
+    {
+        this.gameObject.SetActive(false);
+    }
+
 }
--- Assets/Scripts/Other Managers/GameManager.cs
+++ Assets/Scripts/Other Managers/GameManager.cs
@@ -8,6 +8,12 @@ public class GameManager : MonoBehaviour
 
     public static GameManager Instance;
 
+    [SerializeField]
+    private GameObject _cueStick;
+
+    [SerializeField]
+    private Rigidbody _cueBallRB;
+
     public static event Action onGameStart;
     public static event Action onGameStop;
     public static event Action onAim;
@@ -25,7 +31,25 @@ public class GameManager : MonoBehaviour
 
     }
 
-    
+    private void Start()
+    {
+        StartCoroutine(ICheckCueBallSpeed());
+    }
+
+    private IEnumerator ICheckCueBallSpeed()
+    {
+        while (true)
+        {
+            if (!_cueStick.activeSelf)
+            {
+                if (_cueBallRB.velocity == Vector3.zero)
+                {
+                    _cueStick.SetActive(true);
+                }
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
 
     public void GameStart() => onGameStart?.Invoke();
     public void GameStop() => onGameStop?.Invoke(); 
